bfc1c2b18807be6f29c7e3d0881375980d3c1782
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-15 14:35:34 -0600

    Initial Commit

---
+++ GithubExericse/GithubExericseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubExericseApp.swift
+//  GithubExericse
+//
+//  Created by Mitch Andrade on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubExericseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
