a2e70dc38adb22415afa519fb6662694e8ff6ee9
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-15 14:48:03 -0600

    Version 2.0

--- GithubExericse/ContentView.swift
+++ GithubExericse/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "1.circle")
+            Image(systemName: "2.circle")
                 .resizable()
                 .scaledToFit()
             Text("Version 1.0")
@@ -18,7 +18,7 @@ struct ContentView: View {
                 .fontWeight(.black)
         }
         .padding()
-        .foregroundColor(.mint)
+        .foregroundColor(.blue)
     }
 }
 
